a6d445d7ed1e8042627fd09405ba0ed16f3c8758
unknown <naila.shah777@gmail.com>
2020-05-02 22:09:40 +0530

    adding my first Spring app with view part

--- src/main/java/org/example/springg.java
+++ src/main/java/org/example/springg.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class springg {
     @RequestMapping("/spring")
-    @ResponseBody
 public String getString()
 {
-    return "m spring coded";
+    return "Spring";
 }
 }
